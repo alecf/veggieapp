68c90b7b41b9a2d92594feea2ec30c0a1d7ff1ea
Alec Flett <alecf@flett.org>
2025-01-04 11:20:44 -0800

    add pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,25 @@
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
-
+import { VitePWA } from "vite-plugin-pwa";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      manifest: {
+        name: "Veggie App",
+        short_name: "Veggie App",
+        description: "Veggie App",
+        theme_color: "#ffffff",
+        // icons: [{ src: "/favicon.ico", sizes: "64x64", type: "image/x-icon" }],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
